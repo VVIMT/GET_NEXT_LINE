04bd9d410a0b6d851e6d35eb46eac19609348da0
vinvimo <vinvimo@student.42.fr>
2017-01-05 22:53:53 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,86 +6,98 @@
 /*   By: vinvimo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/03 07:06:31 by vinvimo           #+#    #+#             */
-/*   Updated: 2017/01/03 13:18:05 by vinvimo          ###   ########.fr       */
+/*   Updated: 2017/01/05 22:53:32 by vinvimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static	t_list	*find_file(t_list **file, int fd)
+t_gnl			*ft_newfile(void const *content, size_t fd)
 {
-	t_list *tmp;
+	t_gnl	*file;
+
+	if (!(file = (t_gnl*)malloc(sizeof(t_gnl))))
+		return (NULL);
+	if (content)
+	{
+		file->fd = fd;
+		file->content = ft_memalloc(1);
+		ft_memcpy(file->content, content, 1);
+	}
+	else
+	{
+		file->content = NULL;
+		file->fd = 0;
+	}
+	file->next = NULL;
+	return (file);
+}
+
+static	t_gnl	*find_file(t_gnl **file, int fd)
+{
+	t_gnl	*tmp;
 
 	tmp = *file;
 	while (tmp)
 	{
-		if ((int)tmp->content_size == fd)
+		if (tmp->fd == fd)
 			return (tmp);
 		tmp = tmp->next;
 	}
-	tmp = ft_lstnew("\0", fd);
-	ft_lstadd(file, tmp);
+	tmp = ft_newfile("\0", fd);
+	tmp->next = *file;
+	*file = tmp;
 	tmp = *file;
 	return (tmp);
 }
 
-void			fill_line(int *i, char *tmp, char **line)
-{
-	*i = -1;
-	while (tmp[++*i] && tmp[*i] != '\n')
-		*(*line + *i) = tmp[*i];
-	*(*line + *i) = 0;
-}
-
-int				get_next_line_bis(int fd, int *i, char **line, t_list *lst)
+int				get_next_line_bis(int *i, char *buff, char **line, t_gnl *lst)
 {
 	char	*tmp;
-	char	*buff;
 
-	buff = ft_strnew(BUFF_SIZE + 1);
-	if (BUFF_SIZE <= 0 || read(fd, buff, 0) < 0)
+	if (read(lst->fd, buff, 0) < 0)
 		return (-1);
-	ft_bzero(buff, BUFF_SIZE);
-	while (read(fd, buff, BUFF_SIZE) > 0)
+	while (read(lst->fd, buff, BUFF_SIZE) > 0)
 	{
-		tmp = ft_strdup(lst->content);
+		ERRORCHECK((tmp = ft_strdup(lst->content)));
 		free(lst->content);
-		if (!(lst->content = ft_strjoin(tmp, buff)))
-			return (-1);
+		ERRORCHECK(!(lst->content = ft_strjoin(tmp, buff)));
 		ft_bzero(buff, BUFF_SIZE);
 		free(tmp);
 	}
-	free(buff);
-	tmp = (char*)lst->content;
-	while (tmp[*i] && tmp[*i] != '\n')
+//ft_putendl(lst->content);
+
+	while (lst->content[*i] && lst->content[*i] != '\n')
 		(*i)++;
-	if (!(*line = ft_strnew(*i + 1)))
-		return (-1);
-	fill_line(i, tmp, line);
+	ERRORCHECK((*line = ft_strnew(*i + 1)))
+	*i = -1;
+	while (lst->content[++*i] && lst->content[*i] != '\n')
+		*(*line + *i) = lst->content[*i];
+	*(*line + *i) = 0;
 	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	int				i;
-	char			*tmp;
-	static	t_list	*file;
-	t_list			*lst;
+	char			*buff;
+	static	t_gnl	*file = NULL;
+	t_gnl			*lst;
 
 	i = 0;
-	if (fd < 0 || line == NULL)
+	if (fd < 0 || line == NULL || BUFF_SIZE <= 0)
 		return (-1);
 	lst = find_file(&file, fd);
-	if (get_next_line_bis(fd, &i, line, lst) == -1)
+	ERRORCHECK((buff = ft_strnew(BUFF_SIZE + 1)));
+	ft_bzero(buff, BUFF_SIZE);
+	if (get_next_line_bis(&i, buff, line, lst) == -1)
 		return (-1);
-	if (*((char*)lst->content) == 0)
+	free(buff);
+	if (*(lst->content) == 0)
 		return (0);
-	tmp = ft_strdup(lst->content);
-	ft_strclr(lst->content);
-	if (tmp[i] == 0)
-		ft_strcpy(lst->content, tmp + i);
-	else if (tmp[i] == '\n')
-		ft_strcpy(lst->content, tmp + i + 1);
-	free(tmp);
+//	lst->content = ft_strchr(lst->content, '\n');
+
+	if (*(lst->content) == '\n')
+		(lst->content)++;
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,28 @@
 /*   By: vinvimo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/03 07:06:45 by vinvimo           #+#    #+#             */
-/*   Updated: 2017/01/03 13:20:12 by vinvimo          ###   ########.fr       */
+/*   Updated: 2017/01/05 21:45:47 by vinvimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 999
 
+# define BUFF_SIZE 999
 # include <stdlib.h>
-# include <string.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include "libft/libft.h"
 
-int	get_next_line(const int fd, char **line);
+# define ERRORCHECK(x) if (!x) return (-1);
+
+typedef struct		s_gnl
+{
+	char			*content;
+	int				fd;
+	struct s_gnl	*next;
+}					t_gnl;
+
+int					get_next_line(const int fd, char **line);
 
 #endif
